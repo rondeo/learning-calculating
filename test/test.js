cc79b9ff5d5f6fc0ac24565f6d16f96a1346b5aa
NHQuocThinh <quocthinhag97@gmail.com>
2019-06-16 15:44:19 +0700

    ffd

--- test/test.js
+++ test/test.js
@@ -7,11 +7,6 @@ describe('GET /', function() {   it("should return home page", function(done) {
         .expect("Content-type",/json/)
         .expect(200) // THis is HTTP response
         .end(function(err,res){
-            // HTTP status should be 200
-            //res.status.should.equal(200);
-            //res.status.should.equal(200);
-            // Error key should be false.
-            //res.body.error.should.equal(false);
             if(res.status == 200){
                 done();   
             }
